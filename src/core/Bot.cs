cced3379936e0624b8bf69083b83776c50a76484
Il Harper <afanyiyu@hotmail.com>
2020-11-22 00:52:56 +0800

    feat: Bot Update

--- src/core/Bot.cs
+++ src/core/Bot.cs
@@ -1,7 +1,14 @@
-﻿namespace Kaiheila
+﻿using System;
+using Kaiheila.Events;
+
+namespace Kaiheila
 {
     public class Bot
     {
+        #region Event
 
+        public IObservable<KhEventBase> Event;
+
+        #endregion
     }
 }
