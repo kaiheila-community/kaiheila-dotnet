f999fc3e6d1b0ae373956c118425ac80dd69a8d8
Il Harper <afanyiyu@hotmail.com>
2020-11-22 10:47:28 +0800

    feat: Bot Class Update

--- src/core/Bot.cs
+++ src/core/Bot.cs
@@ -3,10 +3,16 @@ using Kaiheila.Events;
 
 namespace Kaiheila
 {
+    /// <summary>
+    /// Kaiheila机器人。
+    /// </summary>
     public class Bot
     {
         #region Event
 
+        /// <summary>
+        /// 机器人事件Observable。
+        /// </summary>
         public IObservable<KhEventBase> Event;
 
         #endregion
