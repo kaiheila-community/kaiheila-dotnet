4e5820924035dda3468f9408341922f998ae2ed7
Il Harper <afanyiyu@hotmail.com>
2020-11-29 20:55:56 +0800

    refactor: Change Bot to IBot

--- src/core/Client/IBot.cs
+++ src/core/Client/IBot.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using Kaiheila.Events;
 
-namespace Kaiheila
+namespace Kaiheila.Client
 {
     /// <summary>
     /// Kaiheila机器人。
     /// </summary>
-    public class Bot
+    public interface IBot
     {
         #region Event
 
         /// <summary>
         /// 机器人事件Observable。
         /// </summary>
-        public IObservable<KhEventBase> Event;
+        public IObservable<KhEventBase> Event { get; set; }
 
         #endregion
     }
