af2ba72a8578710ee00350dd00f7ea4de28724b8
Il Harper <afanyiyu@hotmail.com>
2020-11-29 22:29:04 +0800

    feat: Add KhFriendsType

---
+++ src/core/Data/KhFriendsType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Kaiheila.Data
+{
+    public enum KhFriendsType
+    {
+        Friend = 0,
+        Request = 1,
+        Blocked = 2
+    }
+
+    public static class KhFriends
+    {
+        public static string GetTypeString(this KhFriendsType type) =>
+            type switch
+            {
+                KhFriendsType.Friend => "friend",
+                KhFriendsType.Request => "request",
+                KhFriendsType.Blocked => "blocked",
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+    }
+}
