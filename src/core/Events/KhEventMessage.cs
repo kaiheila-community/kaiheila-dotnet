10d07ab442f010601fb09a8a4f5fceed7e752348
Il Harper <afanyiyu@hotmail.com>
2020-12-01 13:49:05 +0800

    feat: Add KhEventMessage

---
+++ src/core/Events/KhEventMessage.cs
@@ -0,0 +1,17 @@
+﻿using Kaiheila.Data;
+
+namespace Kaiheila.Events
+{
+    public class KhEventMessage : KhEventBase
+    {
+        public long Guild;
+
+        public long ChannelId;
+
+        public string ChannelName;
+
+        public string Content;
+
+        public KhUser User;
+    }
+}
