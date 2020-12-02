ac727250e4310f9f408d557444b33bec681057e1
Il Harper <afanyiyu@hotmail.com>
2020-12-02 23:44:34 +0800

    feat: Add KhEventTextMessageCombiner

---
+++ src/core/Events/Combiners/KhEventTextMessageCombiner.cs
@@ -0,0 +1,12 @@
+﻿namespace Kaiheila.Events.Combiners
+{
+    [KhEventCombiner(typeof(KhEventTextMessage))]
+    public class KhEventTextMessageCombiner : KhEventCombinerBase
+    {
+        public override KhEventBase Combine(KhEventBase a, KhEventBase b) =>
+            new KhEventTextMessage
+            {
+                Content = (a as KhEventTextMessage)?.Content + (b as KhEventTextMessage)?.Content
+            };
+    }
+}
