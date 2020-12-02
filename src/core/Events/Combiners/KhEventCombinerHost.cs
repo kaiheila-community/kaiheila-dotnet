e5fdc5d2c3795f580803d9b0f56a7bcd126d73c6
Il Harper <afanyiyu@hotmail.com>
2020-12-02 23:51:48 +0800

    feat: Add KhEventCombinerHost

---
+++ src/core/Events/Combiners/KhEventCombinerHost.cs
@@ -0,0 +1,27 @@
+﻿using System.Composition;
+using Microsoft.Extensions.Logging;
+
+namespace Kaiheila.Events.Combiners
+{
+    /// <summary>
+    /// 开黑啦事件合并器主机。
+    /// </summary>
+    [Export]
+    public class KhEventCombinerHost
+    {
+        /// <summary>
+        /// 初始化开黑啦事件合并器主机。
+        /// </summary>
+        /// <param name="logger">开黑啦事件合并器主机日志记录器。</param>
+        public KhEventCombinerHost(
+            ILogger<KhEventCombinerHost> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 开黑啦事件合并器主机日志记录器。
+        /// </summary>
+        private readonly ILogger<KhEventCombinerHost> _logger;
+    }
+}
