a28a6e15c5bb4306948f6bc8064d84bf52fdab33
Il Harper <afanyiyu@hotmail.com>
2020-12-02 23:39:52 +0800

    test: Add KhEventCombiners

---
+++ test/core/Events/Combiners/KhEventCombiners.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Kaiheila.Events.Combiners;
+using Xunit;
+
+namespace Kaiheila.Test.Events.Combiners
+{
+    public class KhEventCombiners
+    {
+        [Fact]
+        public static void KhEventCombinersBasedOnKhEventCombiner()
+        {
+            foreach (Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
+                x.GetTypes()
+                    .Where(type => Attribute.GetCustomAttribute(type, typeof(KhEventCombinerAttribute)) is not null)))
+                Assert.Equal(typeof(KhEventCombinerBase), type.BaseType);
+        }
+    }
+}
