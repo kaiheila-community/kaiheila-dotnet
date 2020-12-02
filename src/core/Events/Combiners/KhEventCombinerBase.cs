1c1c038daf1a96ba7da7ed006b7a09b7ef67e389
Il Harper <afanyiyu@hotmail.com>
2020-12-02 23:31:48 +0800

    feat: Add KhEventCombinerBase

---
+++ src/core/Events/Combiners/KhEventCombinerBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kaiheila.Events.Combiners
+{
+    public abstract class KhEventCombinerBase
+    {
+        public abstract KhEventBase Combine(KhEventBase a, KhEventBase b);
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class KhEventCombinerAttribute : Attribute
+    {
+        public KhEventCombinerAttribute(Type type)
+        {
+            Type = type;
+        }
+
+        public readonly Type Type;
+    }
+}
