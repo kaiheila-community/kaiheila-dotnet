d769f98f9b3cdbf278d5c5818af52c06d02c0348
Il Harper <afanyiyu@hotmail.com>
2020-12-05 15:33:07 +0800

    feat: KhEventFile Update

--- src/core/Events/KhEventFile.cs
+++ src/core/Events/KhEventFile.cs
@@ -4,10 +4,17 @@ namespace Kaiheila.Events
 {
     public class KhEventFile : KhEventAsset
     {
-        public KhEventFile(string path, string name) : base(path, name)
+        public KhEventFile(
+            string path,
+            string name,
+            string extension) : base(path, name)
         {
             if (Type != "attachments")
                 throw new InvalidOperationException($"类型{Type}不是\"attachments\"。");
+
+            Extension = extension;
         }
+
+        public string Extension;
     }
 }
