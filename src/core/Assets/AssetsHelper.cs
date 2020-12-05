54dddda3d4f70e0d75c47059ca37b9d261c8967e
Il Harper <afanyiyu@hotmail.com>
2020-12-05 14:32:34 +0800

    feat: Add AssetsHelper

---
+++ src/core/Assets/AssetsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kaiheila.Assets
+{
+    public static class AssetsHelper
+    {
+        public static string GetAssetType(string path)
+        {
+            string[] split = path.Split('/');
+
+            if (split.Length < 3 || string.IsNullOrEmpty(split[1]))
+                throw new ArgumentOutOfRangeException(nameof(path), "Path不正确。");
+
+            return split[1];
+        }
+    }
+}
