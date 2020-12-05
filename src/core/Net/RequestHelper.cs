09dbac40dffcaebf9028110d91cc81d39236d0cd
Il Harper <afanyiyu@hotmail.com>
2020-12-05 21:57:50 +0800

    feat: RequestHelper Update

--- src/core/Net/RequestHelper.cs
+++ src/core/Net/RequestHelper.cs
@@ -4,12 +4,17 @@ namespace Kaiheila.Net
 {
     public static class RequestHelper
     {
+        public const string UserAgent =
+            @"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) kaiheila/0.0.27 Chrome/80.0.3987.158 Electron/8.2.0 Safari/537.36";
+
+        public const string Accept =
+            @"*/*";
+
         public static HttpWebRequest CreateWebRequest(string uri, bool post = false)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.UserAgent =
-                @"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) kaiheila/0.0.27 Chrome/80.0.3987.158 Electron/8.2.0 Safari/537.36";
-            request.Accept = "*/*";
+            request.UserAgent = UserAgent;
+            request.Accept = Accept;
 
             if (post) request.Method = "POST";
 
