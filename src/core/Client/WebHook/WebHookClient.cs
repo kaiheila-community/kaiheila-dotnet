af9cc2570753e3833815802177b2fc9b90fe2216
Il Harper <afanyiyu@hotmail.com>
2020-12-24 14:56:11 +0800

    feat: Create WebHookClient

---
+++ src/core/Client/WebHook/WebHookClient.cs
@@ -0,0 +1,17 @@
+﻿using Kaiheila.Client.Rest;
+
+namespace Kaiheila.Client.WebHook
+{
+    public class WebHookClient : RestClient
+    {
+        #region Constructor
+
+        internal WebHookClient(WebHookClientOptions options) : base(options)
+        {
+        }
+
+        public static WebHookClientBuilder CreateWebHookClient() => new WebHookClientBuilder();
+
+        #endregion
+    }
+}
---
+++ src/core/Client/WebHook/WebHookClientExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Kaiheila.Client.Rest;
+using Newtonsoft.Json;
+
+namespace Kaiheila.Client.WebHook
+{
+    [Serializable]
+    [JsonObject(MemberSerialization.OptIn)]
+    public class WebHookClientOptions : RestClientOptions
+    {
+        [Required]
+        [JsonProperty("verify_token")]
+        public string VerifyToken { get; set; } = "";
+
+        [JsonProperty("encrypt_key")]
+        public string EncryptKey { get; set; } = "";
+    }
+
+    public class WebHookClientBuilder
+    {
+        internal readonly WebHookClientOptions Options = new WebHookClientOptions();
+
+        public WebHookClientBuilder Configure(
+            Action<WebHookClientOptions> configureClient)
+        {
+            configureClient(Options);
+            return this;
+        }
+    }
+
+    public static class WebHookClientExtensions
+    {
+        #region Options Builder
+
+        public static WebHookClientOptions UseVerifyToken(
+            this WebHookClientOptions options,
+            string verifyToken)
+        {
+            options.VerifyToken = verifyToken;
+            return options;
+        }
+
+        public static WebHookClientOptions UseEncryptKey(
+            this WebHookClientOptions options,
+            string encryptKey)
+        {
+            options.EncryptKey = encryptKey;
+            return options;
+        }
+
+        #endregion
+    }
+}
