6e9a4bcf366a1593c2d86f11739f61a4bd621bb5
Il Harper <afanyiyu@hotmail.com>
2020-12-26 02:34:36 +0800

    feat: EventParserHost Update

--- src/core/Events/EventParserHost.cs
+++ src/core/Events/EventParserHost.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Composition;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json.Linq;
@@ -16,12 +19,53 @@ namespace Kaiheila.Events
             ILogger<EventParserHost> logger)
         {
             _logger = logger ?? new NullLogger<EventParserHost>();
+
+            foreach (Type type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(x =>
+                x.GetTypes()
+                    .Where(type => Attribute.GetCustomAttribute(type, typeof(KhEventAttribute)) is not null)))
+            {
+                string eventType = (Attribute.GetCustomAttribute(type, typeof(KhEventAttribute)) as KhEventAttribute)?.Type;
+                if (eventType == null || _eventBases.ContainsKey(eventType) || type.FullName == null) continue;
+
+                _eventBases.Add(eventType, type);
+            }
+
+            _logger.LogInformation($"加载了{_eventBases.Count}个Kaiheila事件类型。");
         }
 
-        public KhEventBase Parse(JToken payload)
+        public async Task<KhEventBase> Parse(JToken payload)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // ReSharper disable PossibleNullReferenceException
+
+                string eventType = $"{payload["channel_type"].ToObject<string>()}|{payload["type"].ToObject<int>()}";
+                if (!_eventBases.ContainsKey(eventType))
+                    throw new EventParseException($"Cannot find event parser for type {eventType}.", payload);
+
+                KhEventBase eventBase = (Activator.CreateInstance(_eventBases[eventType]) as KhEventBase);
+                await eventBase.Parse(payload);
+
+                eventBase.Id = payload["msg_id"].ToObject<string>();
+                eventBase.TargetId = payload["msg_timestamp"].ToObject<long>();
+                eventBase.Content = payload["content"].ToObject<string>();
+                eventBase.TargetId = payload["target_id"].ToObject<long>();
+
+                return eventBase;
+
+                // ReSharper restore PossibleNullReferenceException
+            }
+            catch (EventParseException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new EventParseException("Error when parsing event.", payload, e);
+            }
         }
+
+        private readonly Dictionary<string, Type> _eventBases = new Dictionary<string, Type>();
 
         private ILogger<EventParserHost> _logger;
     }
