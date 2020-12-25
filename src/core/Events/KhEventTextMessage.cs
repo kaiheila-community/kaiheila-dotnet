6a1cfc83df1a0667a3d124445c39f2db50079c09
Il Harper <afanyiyu@hotmail.com>
2020-12-26 02:20:44 +0800

    feat: KhEventTextMessage Update

--- src/core/Events/KhEventTextMessage.cs
+++ src/core/Events/KhEventTextMessage.cs
@@ -1,10 +1,19 @@
 ﻿using System.Threading.Tasks;
 using Kaiheila.Client;
+using Kaiheila.Data;
+using Newtonsoft.Json.Linq;
 
 namespace Kaiheila.Events
 {
-    public class KhEventTextMessage : KhEventBase
+    public class KhEventTextMessage : KhEventBase, IKhEventUser
     {
+        public override async Task Parse(JToken payload)
+        {
+            User.Id = payload["author"]["id"].ToObject<long>();
+            User.Username = payload["author"]["username"].ToObject<string>();
+            User.Nickname = payload["author"]["nickname"].ToObject<string>();
+        }
+
         public override async Task Send(Bot bot)
         {
             var (msgId, msgTimestamp) = await bot.SendTextMessage(
@@ -15,5 +24,7 @@ namespace Kaiheila.Events
             Id = msgId;
             Timestamp = msgTimestamp;
         }
+
+        public KhUser User { get; set; } = new KhUser();
     }
 }
